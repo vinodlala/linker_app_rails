dbc029c2c78551e7c0286b1429d0ffb4cb3c49ac
Vinod Lala <vinod_lala@usa.net>
2013-10-07 14:53:11 -0400

    first time

---
+++ app/models/link_item.rb
@@ -0,0 +1,5 @@
+class LinkItem < ActiveRecord::Base
+  attr_accessible :needs_email, :original_url, :password, :short_name
+
+  has_many :visitors
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LinkerAppRails::Application.config.secret_token = 'f3fc7679de5833e97ab1d935c8a2b7838d3d32986b6ad0b750c60001b5a67041abea9d86bc939328bdb4bfa2b31059e65a8ac5ad89c61d408bbd3ecb3251a9d6'
